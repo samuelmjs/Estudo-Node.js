bdfcb9dfda73d27368c9e3394547fcf26cacefd6
Samuel <samuel.jesus@martinbrower.com.br>
2019-10-28 11:59:14 -0300

    Insere busca users

--- Modulo 1/index.js
+++ Modulo 1/index.js
@@ -4,9 +4,18 @@ const server = express();
 
 server.use(express.json());
 
+const users = ['Samuel', 'Thiago', 'Lucas'];
 
+//busca todos os usuários
 server.get('/users', (req, res) => {
-    return res.json({ message: 'ola'});
+    return res.json(users);
+})
+
+//busca único usuário
+server.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(users[index]);
 })
 
 server.listen(3000);
