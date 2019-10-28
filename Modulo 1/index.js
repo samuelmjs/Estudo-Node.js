1cad4f69f34bfe9864b40bc3cc0faf5b5919c9d9
Samuel <samuel.jesus@martinbrower.com.br>
2019-10-28 13:43:29 -0300

    insere rota criar user

--- Modulo 1/index.js
+++ Modulo 1/index.js
@@ -9,13 +9,22 @@ const users = ['Samuel', 'Thiago', 'Lucas'];
 //busca todos os usuários
 server.get('/users', (req, res) => {
     return res.json(users);
-})
+});
 
 //busca único usuário
 server.get('/users/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(users[index]);
+});
+
+//criar usuário
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
 })
 
 server.listen(3000);
