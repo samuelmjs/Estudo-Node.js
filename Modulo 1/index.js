df346a6a2afe9994725b219e0e02face93b90d90
Samuel <samuel.jesus@martinbrower.com.br>
2019-10-28 14:53:18 -0300

    Middlewares validação users

--- Modulo 1/index.js
+++ Modulo 1/index.js
@@ -6,20 +6,52 @@ server.use(express.json());
 
 const users = ['Samuel', 'Thiago', 'Lucas'];
 
+//Middleware Global
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+//Middleware Local. Verificar body
+function checkUserExists(req, res, next) {
+    const { name } = req.body
+    if (!name) {
+        return res.status(400).json({ error: 'User name is required' });
+    }
+
+    return next();
+}
+
+//Middleware Local. Verifica usuário no array
+function checkUserInArray(req, res, next) {
+    const { index } = req.params;
+    const user = users[index];
+
+    if (!user) {
+        return res.status(400).json({ error: 'User does not exist' });
+    }
+
+    req.user = user;
+
+    return next();
+}
+
 //busca todos os usuários
 server.get('/users', (req, res) => {
     return res.json(users);
 });
 
 //busca único usuário
-server.get('/users/:index', (req, res) => {
-    const { index } = req.params;
-
-    return res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user);
 });
 
 //criar usuário
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
     const { name } = req.body;
 
     users.push(name);
@@ -28,7 +60,7 @@ server.post('/users', (req, res) => {
 });
 
 //alterar usuário
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
 
@@ -38,7 +70,7 @@ server.put('/users/:index', (req, res) => {
 });
 
 //deletar usuário
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
     const { index } = req.params;
 
     users.splice(index, 1);
