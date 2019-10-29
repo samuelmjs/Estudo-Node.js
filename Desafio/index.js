dd184954ba490ba822eb84b2382277ce75b198ee
Samuel <samuel.jesus@martinbrower.com.br>
2019-10-29 09:37:48 -0300

    Criação Rotas

--- Desafio/index.js
+++ Desafio/index.js
@@ -4,8 +4,56 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    return res.send('ola');
+const projects = [];
+
+server.get('/projects', (req, res) => {
+    return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+
+    const project = {
+        id,
+        title,
+        tasks: []
+    };
+
+    projects.push(project);
+
+    return res.json(projects);
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const project = projects.find(project => project.id === id);
+
+    project.tasks.push(title);
+
+    return res.json(project);
+});
+
+server.put('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const project = projects.find(project => project.id === id);
+
+    project.title = title;
+
+    return res.json(project);
+});
+
+server.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+
+    const indexProject = projects.findIndex(project => project.id === id);
+
+    projects.splice(indexProject, 1);
+
+    return res.send();
 });
 
 server.listen(3000);
