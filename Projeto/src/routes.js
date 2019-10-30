b4f699ec332ae2dbce81cbf7dd92bfdbc25d931e
Samuel <samuel.jesus@martinbrower.com.br>
2019-10-30 08:37:45 -0300

    configure eslint prettier sequelize

--- Projeto/src/routes.js
+++ Projeto/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'hellow world' });
+  return res.json({ message: 'hellow world' });
 });
 
 export default routes;
