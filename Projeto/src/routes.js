2877b602b517dbade2f14e68e98c0115efc06f84
Samuel <samuel.jesus@martinbrower.com.br>
2019-11-05 09:36:36 -0300

    Insere campo avatar na tabela usuarios

--- Projeto/src/routes.js
+++ Projeto/src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/fileController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -17,8 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', authMiddleware, UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
